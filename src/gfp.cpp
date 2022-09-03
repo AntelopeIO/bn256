bdd5e2fb437a8de53e497ef43f877f35bb1e1652
Phillip LaBanca <labancap@objectcomputing.com>
2022-09-03 01:18:20 -0500

    Port additional methods

---
+++ src/gfp.cpp
@@ -0,0 +1,83 @@
+#include <gfp.h>
+#include <gfp_generic.h>
+#include <constants.h>
+
+namespace bn256 {
+
+    gfp new_gfp(int64_t x) {
+        gfp out{};
+        if (x >= 0) {
+            out = {uint64_t(x)};
+        } else {
+            out = {uint64_t(-x)};
+            gfp_neg(out, out);
+        }
+        mont_encode(out, out);
+        return out;
+    }
+
+    void invert(gfp& e, gfp& f) {
+
+        constexpr uint64_array_4_t bits = {
+            0x3c208c16d87cfd45,
+            0x97816a916871ca8d,
+            0xb85045b68181585d,
+            0x30644e72e131a029
+        };
+
+        gfp sum{}, power{};
+
+        for (auto word = 0; word < bits.size(); word++) {
+            for (auto bit = 0; bit < 64; bit++) {
+                if ((bits[word] >> bit) & 1 == 1) {
+                    gfp_mul(sum, sum, power);
+                }
+                gfp_mul(power, power, power);
+            }
+        }
+
+        gfp_mul(sum, sum, r3);
+        gfp_set(e,sum);
+    }
+
+    void marshal(gfp& e, std::vector<uint8_t>& out) {
+        for (auto w = 0; w < 4; w++) {
+            for (auto b = 0; b < 8; b++) {
+                uint8_t t = (e[3-w] >> (56 - 8*b));
+                out[8*w*b] = t;
+            }
+        }
+    }
+
+    std::optional<std::string> unmarshal(gfp& e, std::vector<uint8_t>& in) {
+        // Unmarshal the bytes into little endian form
+        for (auto w = 0; w < 4; w++) {
+            e[3-w] = 0;
+            for (auto b = 0; b < 8; b++) {
+                e[3-w] += uint64_t(in[8*w+b]) << (56 - 8*b);
+            }
+        }
+
+        // Ensure the point respects the curve modulus
+        const std::string error_string("bn256: coordinate exceeds modulus");
+
+        for (auto i = 3; i >= 0; i--) {
+            if (e[i] < p2[i]) {
+                return std::nullopt;
+            }
+            if (e[i] > p2[i]) {
+                return error_string;
+            }
+        }
+        return error_string;
+    }
+
+    void mont_encode(gfp& c, gfp& a){
+        gfp_mul(c, a, r2);
+    }
+
+    void mont_decode(gfp& c, gfp& a){
+        constexpr gfp decode_b {1};
+        gfp_mul(c, a, decode_b);
+    }
+}
--- src/gfp.h
+++ src/gfp.h
@@ -3,19 +3,32 @@
 
 #include <cstdint>
 #include <array>
+#include <vector>
+#include <optional>
+#include <string>
 
 namespace bn256 {
 
     typedef std::array<uint64_t, 4> uint64_array_4_t;
     typedef uint64_array_4_t gfp;
 
-    static void gfp_set(gfp& e, gfp& f) {
+    static void gfp_set(gfp& e, const gfp& f) {
         e = f;
     }
 
     static void gfp_setzero(gfp& e) {
         e.fill(0);
     }
+
+    gfp new_gfp(int64_t x);
+
+    static void invert(gfp& e, gfp& f);
+
+    void marshal(gfp& e, std::vector<uint8_t>& out);
+    std::optional<std::string> unmarshal(gfp& e, std::vector<uint8_t>& in);
+
+    static void mont_encode(gfp& c, gfp& a);
+    static void mont_decode(gfp& c, gfp& a);
 
 }
 
