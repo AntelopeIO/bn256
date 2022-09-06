5049e0c1bc20a362f59688c9e4d729c7af2877aa
Phillip LaBanca <labancap@objectcomputing.com>
2022-09-05 23:07:04 -0500

    Add twist methods

---
+++ src/twist.h
@@ -0,0 +1,39 @@
+#ifndef BN256_TWIST_H
+#define BN256_TWIST_H
+
+#include <gfp2.h>
+#include <boost/multiprecision/cpp_int.hpp>
+
+using boost::multiprecision::int128_t;
+
+namespace bn256 {
+    struct twist_point {
+        gfp2 x_;
+        gfp2 y_;
+        gfp2 z_;
+        gfp2 t_;
+
+        void set(twist_point& a);
+
+        bool is_on_curve();
+
+        void set_infinity();
+
+        [[nodiscard]] bool is_infinity() const;
+
+        void add(twist_point& a, twist_point& b);
+
+        void double_(twist_point& a);
+
+        void mul(twist_point& a, const int128_t& scalar);
+
+        void make_affine();
+
+        void neg(twist_point& a);
+
+    };
+
+}
+
+
+#endif //BN256_TWIST_H
