4f92eb28c31d788e202044346614cfc51f946bb5
Phillip LaBanca <labancap@objectcomputing.com>
2022-09-19 08:54:39 -0500

    Refactor for ctest

--- test/lattice_test.cpp
+++ test/lattice_test.cpp
@@ -1,90 +1,87 @@
 #include <lattice.h>
-#include <random>
 #include <iostream>
 #include <iosfwd>
 #include <bitlen.h>
+#include <random_256.h>
 
-using boost::multiprecision::int256_t;
+using namespace boost::multiprecision::literals;
+using namespace boost::multiprecision;
 
-void test_lattice_reduce_curve() {
+constexpr int test_fail = 1;
+constexpr int test_pass = 0;
+
+int test_lattice_reduce_curve() {
+    int rc = test_pass;
     constexpr std::size_t max_bitlen = 130;
-    // std::srand(bn256::order);
-    auto ks = bn256::curve_lattice.decompose(3);
+    bn256::random_256 rand;
+    auto k = rand.sample();
+    std::cout << "test_lattice_reduce_curve: " << k << std::endl;
+    auto ks = bn256::curve_lattice.decompose(k);
     bool too_large = false;
     bool not_positive = false;
     for (std::size_t i = 0; i < 2; i++) {
         auto bit_len = bn256::bitlen(ks[i]);
-        std::cout << __FILE__ << ":"
-                  << __LINE__ << ","
-                  << i << ","
+        std::cout << i << ","
+                  << ks[i] << ","
                   << bit_len << ","
-                  << ks[i] << std::endl;
+                  << ks[i].sign() << std::endl;
         if(bit_len > max_bitlen) {
             too_large = true;
-            std::cout << "Large::[" << __FILE__ << ":"
-                      << __LINE__ << ","
-                      << i << ","
-                      << bit_len << ","
-                      << ks[i] << "]" << std::endl;
         }
         if(ks[i].sign() < 0 ) {
             not_positive = true;
-            std::cout << "Negative::[" << __FILE__ << ":"
-                      << __LINE__ << ","
-                      << i << ","
-                      << bit_len << ","
-                      << ks[i] << "]" << std::endl;
         }
     }
     if (too_large) {
         std::cout << "reduction too large" << std::endl;
+        rc = test_fail;
     }
     if (not_positive) {
         std::cout << "reduction must be positive" << std::endl;
+        rc = test_fail;
     }
+    std::cout << std::endl << std::endl;
+    return rc;
 }
 
-void test_lattice_reduce_target() {
+int test_lattice_reduce_target() {
+    int rc = test_pass;
     constexpr std::size_t max_bitlen = 66;
-    // std::srand(bn256::order);
-    auto ks = bn256::target_lattice.decompose(3);
+    bn256::random_256 rand;
+//    auto k = rand.sample();
+    auto k = 0x3E7AA0B35C1123515E051CF20868560F8A19CE2B009E81BF4A5151A899D0599_cppi512;
+    std::cout << "test_lattice_reduce_target: " << k << std::endl;
+    auto ks = bn256::target_lattice.decompose(k);
     bool too_large = false;
     bool not_positive = false;
     for (std::size_t i = 0; i < 4; i++) {
         auto bit_len = bn256::bitlen(ks[i]);
-        std::cout << __FILE__ << ":"
-                  << __LINE__ << ","
-                  << i << ","
+        std::cout << i << ","
+                  << ks[i] << ","
                   << bit_len << ","
-                  << ks[i] << std::endl;
+                  << ks[i].sign() << std::endl;
         if(bit_len > max_bitlen) {
             too_large = true;
-            std::cout << "Large::[" << __FILE__ << ":"
-                      << __LINE__ << ","
-                      << i << ","
-                      << bit_len << ","
-                      << ks[i] << "]" << std::endl;
         }
         if(ks[i].sign() < 0 ) {
             not_positive = true;
-            std::cout << "Negative::[" << __FILE__ << ":"
-                      << __LINE__ << ","
-                      << i << ","
-                      << bit_len << ","
-                      << ks[i] << "]" << std::endl;
         }
     }
     if (too_large) {
         std::cout << "reduction too large" << std::endl;
+        rc = test_fail;
     }
     if (not_positive) {
         std::cout << "reduction must be positive" << std::endl;
+        rc = test_fail;
     }
-
+    std::cout << std::endl << std::endl;
+    return rc;
 }
 
 int main() {
-    test_lattice_reduce_curve();
-    test_lattice_reduce_target();
-    return 0;
+    int rc = test_pass;
+    rc += test_lattice_reduce_curve();
+    rc += test_lattice_reduce_target();
+    return rc;
 }
