7c23e02752b06f067c2d53cca9fba43f3cca61b9
Huang-Ming Huang <huangh@objectcomputing.com>
2022-10-14 12:01:31 -0500

    clang intrinsics

--- src/gfp_generic.h
+++ src/gfp_generic.h
@@ -17,7 +17,25 @@ namespace constants {
 
 constexpr array<uint64_t, 4> gfp_carry(const array<uint64_t, 4>& a, uint64_t head) noexcept {
    array<uint64_t, 4> b{};
-   uint64_t           carry = {};
+
+#if defined(__clang__) && __clang_major__ > 8
+   if (!__builtin_is_constant_evaluated()) {
+      unsigned long long carryin = 0, carryout = 0;
+      b[0]    = __builtin_subcll(a[0], constants::p2[0], carryin, &carryout);
+      carryin = carryout;
+      b[1]    = __builtin_subcll(a[1], constants::p2[1], carryin, &carryout);
+      carryin = carryout;
+      b[2]    = __builtin_subcll(a[2], constants::p2[2], carryin, &carryout);
+      carryin = carryout;
+      b[3]    = __builtin_subcll(a[3], constants::p2[3], carryin, &carryout);
+      carryin = carryout;
+      __builtin_subcll(head, 0, carryin, &carryout);
+
+      return carryout ? a : b;
+   }
+#endif
+
+   uint64_t carry = {};
 
    carry = __builtin_sub_overflow(a[0], constants::p2[0], &b[0]);
    carry = __builtin_sub_overflow(a[1], carry, &b[1]);
@@ -32,8 +50,21 @@ constexpr array<uint64_t, 4> gfp_carry(const array<uint64_t, 4>& a, uint64_t hea
 }
 
 constexpr array<uint64_t, 4> gfp_neg(const array<uint64_t, 4>& a) noexcept {
-   uint64_t           carry = 0;
    array<uint64_t, 4> b{};
+#if defined(__clang__) && __clang_major__ > 8
+   if (!__builtin_is_constant_evaluated()) {
+      unsigned long long carryin = 0, carryout = 0;
+      b[0]    = __builtin_subcll(constants::p2[0], a[0], carryin, &carryout);
+      carryin = carryout;
+      b[1]    = __builtin_subcll(constants::p2[1], a[1], carryin, &carryout);
+      carryin = carryout;
+      b[2]    = __builtin_subcll(constants::p2[2], a[2], carryin, &carryout);
+      carryin = carryout;
+      b[3]    = __builtin_subcll(constants::p2[3], a[3], carryin, &carryout);
+      return gfp_carry(b, 0);
+   }
+#endif
+   uint64_t carry = 0;
 
    carry = __builtin_sub_overflow(constants::p2[0], a[0], &b[0]);
    carry = __builtin_sub_overflow(constants::p2[1], carry, &b[1]);
@@ -46,8 +77,22 @@ constexpr array<uint64_t, 4> gfp_neg(const array<uint64_t, 4>& a) noexcept {
 }
 
 constexpr array<uint64_t, 4> gfp_add(const array<uint64_t, 4>& a, const array<uint64_t, 4>& b) noexcept {
-   uint64_t           carry = 0;
    array<uint64_t, 4> c{};
+
+#if defined(__clang__) && __clang_major__ > 8
+   if (!__builtin_is_constant_evaluated()) {
+      unsigned long long carryin = 0, carryout = 0;
+      c[0]    = __builtin_addcll(a[0], b[0], carryin, &carryout);
+      carryin = carryout;
+      c[1]    = __builtin_addcll(a[1], b[1], carryin, &carryout);
+      carryin = carryout;
+      c[2]    = __builtin_addcll(a[2], b[2], carryin, &carryout);
+      carryin = carryout;
+      c[3]    = __builtin_addcll(a[3], b[3], carryin, &carryout);
+      return gfp_carry(c, carryout);
+   }
+#endif
+   uint64_t carry = 0;
 
    carry = __builtin_add_overflow(a[0], b[0], &c[0]);
    carry = __builtin_add_overflow(a[1], carry, &c[1]);
@@ -61,8 +106,34 @@ constexpr array<uint64_t, 4> gfp_add(const array<uint64_t, 4>& a, const array<ui
 }
 
 constexpr array<uint64_t, 4> gfp_sub(const array<uint64_t, 4>& a, const array<uint64_t, 4>& b) noexcept {
-   uint64_t           carry = 0;
    array<uint64_t, 4> t{};
+
+#if defined(__clang__) && __clang_major__ > 8
+   if (!__builtin_is_constant_evaluated()) {
+      unsigned long long carryin = 0, carryout = 0;
+      t[0]    = __builtin_subcll(constants::p2[0], b[0], carryin, &carryout);
+      carryin = carryout;
+      t[1]    = __builtin_subcll(constants::p2[1], b[1], carryin, &carryout);
+      carryin = carryout;
+      t[2]    = __builtin_subcll(constants::p2[2], b[2], carryin, &carryout);
+      carryin = carryout;
+      t[3]    = __builtin_subcll(constants::p2[3], b[3], carryin, &carryout);
+
+      carryin = 0;
+      array<uint64_t, 4> c{};
+
+      c[0]    = __builtin_addcll(a[0], t[0], carryin, &carryout);
+      carryin = carryout;
+      c[1]    = __builtin_addcll(a[1], t[1], carryin, &carryout);
+      carryin = carryout;
+      c[2]    = __builtin_addcll(a[2], t[2], carryin, &carryout);
+      carryin = carryout;
+      c[3]    = __builtin_addcll(a[3], t[3], carryin, &carryout);
+
+      return gfp_carry(c, carryout);
+   }
+#endif
+   uint64_t carry = 0;
 
    carry = __builtin_sub_overflow(constants::p2[0], b[0], &t[0]);
    carry = __builtin_sub_overflow(constants::p2[1], carry, &t[1]);
@@ -88,15 +159,17 @@ constexpr array<uint64_t, 4> gfp_sub(const array<uint64_t, 4>& a, const array<ui
 
 constexpr array<uint64_t, 8> full_mul(const array<uint64_t, 4>& a, const array<uint64_t, 4>& b) noexcept {
    array<uint64_t, 9> c{};
-   auto add_to_c = [&c](__int128_t x, int index) {
-        uint64_t upper = x >> 64;
-        uint64_t carry = __builtin_add_overflow( c[index], static_cast<uint64_t>(x), &c[index]);
-        carry = __builtin_add_overflow(c[index+1], carry, &c[index+1]);
-        carry += __builtin_add_overflow(upper, c[index+1], &c[index+1]);
-        __builtin_add_overflow(c[index+2], carry, &c[index+2]);
-    };
+   auto               add_to_c = [&c](__int128_t x, int index) {
+      uint64_t upper = x >> 64;
+      uint64_t carry = __builtin_add_overflow(c[index], static_cast<uint64_t>(x), &c[index]);
+      carry          = __builtin_add_overflow(c[index + 1], carry, &c[index + 1]);
+      carry += __builtin_add_overflow(upper, c[index + 1], &c[index + 1]);
+      __builtin_add_overflow(c[index + 2], carry, &c[index + 2]);
+   };
 
+   #pragma GCC unroll 4
    for (int i = 0; i < 4; ++i)
+   #pragma GCC unroll 4
       for (int j = 0; j < 4; ++j) { add_to_c(static_cast<__uint128_t>(b[i]) * a[j], i + j); }
    return { c[0], c[1], c[2], c[3], c[4], c[5], c[6], c[7] };
 }
@@ -111,7 +184,9 @@ constexpr array<uint64_t, 4> half_mul(const array<uint64_t, 4>& a, const array<u
       __builtin_add_overflow(c[index + 2], carry, &c[index + 2]);
    };
 
+   #pragma GCC unroll 4
    for (int i = 0; i < 4; ++i)
+   #pragma GCC unroll 4
       for (int j = 0; j < 4; ++j) {
          if ((i + j) > 3)
             break;
@@ -125,7 +200,30 @@ constexpr array<uint64_t, 4> gfp_mul(const array<uint64_t, 4>& a, const array<ui
    auto m = half_mul({ T[0], T[1], T[2], T[3] }, constants::np);
    auto t = full_mul(m, constants::p2);
 
+#if defined(__clang__) && __clang_major__ > 8
+   if (!__builtin_is_constant_evaluated()) {
+      unsigned long long carryin = 0, carryout = 0;
+      T[0]    = __builtin_addcll(T[0], t[0], carryin, &carryout);
+      carryin = carryout;
+      T[1]    = __builtin_addcll(T[1], t[1], carryin, &carryout);
+      carryin = carryout;
+      T[2]    = __builtin_addcll(T[2], t[2], carryin, &carryout);
+      carryin = carryout;
+      T[3]    = __builtin_addcll(T[3], t[3], carryin, &carryout);
+      carryin = carryout;
+      T[4]    = __builtin_addcll(T[4], t[4], carryin, &carryout);
+      carryin = carryout;
+      T[5]    = __builtin_addcll(T[5], t[5], carryin, &carryout);
+      carryin = carryout;
+      T[6]    = __builtin_addcll(T[6], t[6], carryin, &carryout);
+      carryin = carryout;
+      T[7]    = __builtin_addcll(T[7], t[7], carryin, &carryout);
+      return gfp_carry({ T[4], T[5], T[6], T[7] }, carryout);
+   }
+#endif
+
    uint64_t carry = 0;
+
    carry = __builtin_add_overflow(T[0], t[0], &T[0]);
    carry = __builtin_add_overflow(T[1], carry, &T[1]);
    carry += __builtin_add_overflow(T[1], t[1], &T[1]);
