d4ea143d3d9335137b7feb39c4df0b5576320718
Huang-Ming Huang <huangh@objectcomputing.com>
2023-02-07 09:21:31 -0600

    detect bmi2

---
+++ src/mul_u256_bmi2.cpp
@@ -0,0 +1,15 @@
+#if defined (__ELF__)
+#define BN256_MUL_U256_IMPL_ONLY
+#include "bitint_arithmetic.h"
+// should compiled with -mbmi2 option
+
+namespace bn256 {
+void full_mul_u256_bmi2(const uint64_t* a, const uint64_t* b, uint64_t* c) noexcept{
+    return full_mul_u256_impl(a, b, c);
+}
+
+void half_mul_u256_bmi2(const uint64_t* a, const uint64_t* b, uint64_t* c) noexcept {
+    return half_mul_u256_impl(a, b, c);
+}
+}
+#endif
